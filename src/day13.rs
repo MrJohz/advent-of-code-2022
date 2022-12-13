ff035f8e0d720b6776ebc05a7f753db5d1bfad13
Jonathan Frere <jonathan.frere@esveo.com>
2022-12-13 16:14:40 +0100

    Optimise parsing routines

--- src/day13.rs
+++ src/day13.rs
@@ -1,13 +1,14 @@
+use std::str::FromStr;
+
 use itertools::Itertools;
 use rayon::{
-    prelude::{IndexedParallelIterator, IntoParallelIterator, ParallelExtend, ParallelIterator},
+    prelude::{ParallelBridge, ParallelIterator},
     slice::ParallelSliceMut,
-    str::ParallelString,
 };
 
 pub struct Day13;
 
-#[derive(Debug, Clone, PartialEq, Eq, Ord, serde::Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, serde::Deserialize)]
 #[serde(untagged)]
 enum Data {
     Array(Vec<Data>),
@@ -16,33 +17,80 @@ enum Data {
 
 impl PartialOrd for Data {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Data {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         match (self, other) {
-            (Self::Integer(n1), Self::Integer(n2)) => n1.partial_cmp(n2),
-            (Self::Array(a1), Self::Array(a2)) => a1.partial_cmp(a2),
-            (Self::Array(a1), Self::Integer(n2)) => a1.partial_cmp(&vec![Self::Integer(*n2)]),
-            (Self::Integer(n1), Self::Array(a2)) => vec![Self::Integer(*n1)].partial_cmp(a2),
+            (Self::Integer(n1), Self::Integer(n2)) => n1.cmp(n2),
+            (Self::Array(a1), Self::Array(a2)) => a1.cmp(a2),
+            (Self::Array(a1), Self::Integer(n2)) => a1.cmp(&vec![Self::Integer(*n2)]),
+            (Self::Integer(n1), Self::Array(a2)) => vec![Self::Integer(*n1)].cmp(a2),
         }
+    }
+}
+
+impl Data {
+    fn create_int_from_bytes(start_idx: usize, bytes: &[u8]) -> (Self, usize) {
+        match (bytes[start_idx], bytes[start_idx + 1]) {
+            (b'1', b'0') => (Self::Integer(10), start_idx + 2),
+            (c, _) => (Self::Integer((c - b'0') as usize), start_idx + 1),
+        }
+    }
+
+    fn create_array_from_bytes(mut start_idx: usize, bytes: &[u8]) -> (Self, usize) {
+        start_idx += 1;
+        let mut elems = Vec::new();
+
+        loop {
+            match bytes[start_idx] {
+                b']' => return (Self::Array(elems), start_idx + 1),
+                b'0'..=b'9' => {
+                    let (elem, idx) = Self::create_int_from_bytes(start_idx, bytes);
+                    elems.push(elem);
+                    start_idx = idx;
+                    continue;
+                }
+                b'[' => {
+                    let (elem, idx) = Self::create_array_from_bytes(start_idx, bytes);
+                    elems.push(elem);
+                    start_idx = idx;
+                    continue;
+                }
+                _ => start_idx += 1,
+            }
+        }
+    }
+}
+
+impl FromStr for Data {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bytes = &s.as_bytes();
+
+        let (me, _) = Self::create_array_from_bytes(0, bytes);
+
+        Ok(me)
     }
 }
 
 impl crate::runner::Day for Day13 {
     fn part_1(input: &str) -> anyhow::Result<String> {
-        let lines = input
+        let sum = input
             .lines()
             .filter(|l| !l.is_empty())
-            .map(|s| s.as_bytes().to_vec())
             .tuples()
-            .collect::<Vec<_>>();
-
-        let sum = lines
-            .into_par_iter()
-            .map(|(mut s1, mut s2)| {
+            .enumerate()
+            .par_bridge()
+            .map(|(idx, (s1, s2))| {
                 (
-                    simd_json::from_slice::<Data>(s1.as_mut()).unwrap(),
-                    simd_json::from_slice::<Data>(s2.as_mut()).unwrap(),
+                    idx,
+                    (s1.parse::<Data>().unwrap(), s2.parse::<Data>().unwrap()),
                 )
             })
-            .enumerate()
             .filter(|(_, (left, right))| left < right)
             .map(|(idx, _)| idx + 1)
             .sum::<usize>();
@@ -56,14 +104,13 @@ impl crate::runner::Day for Day13 {
         let divider_2 = Data::Array(vec![Data::Array(vec![Data::Integer(2)])]);
         let divider_6 = Data::Array(vec![Data::Array(vec![Data::Integer(6)])]);
         let mut items = vec![divider_2.clone(), divider_6.clone()];
-        items.par_extend(
+        items.extend(
             input
-                .par_lines()
+                .lines()
                 .filter(|l| !l.is_empty())
-                .map(|s| s.as_bytes().to_vec())
-                .map(|mut each| simd_json::from_slice::<Data>(each.as_mut()).unwrap()),
+                .map(|each| each.parse().unwrap()),
         );
-        items.par_sort();
+        items.par_sort_unstable();
 
         let sum = items
             .into_iter()
@@ -85,47 +132,32 @@ mod tests {
 
     #[test]
     fn ordering_data_integers_works_as_expected() {
-        assert_eq!(Integer(1) < Integer(2), true);
-        assert_eq!(Integer(2) == Integer(2), true);
+        assert!(Integer(1) < Integer(2));
+        assert!(Integer(2) == Integer(2));
     }
 
     #[test]
     fn ordering_equal_sized_data_arrays_works_as_expected() {
-        assert_eq!(Array(vec![Integer(1)]) < Array(vec![Integer(2)]), true);
-        assert_eq!(Array(vec![Integer(2)]) == Array(vec![Integer(2)]), true);
+        assert!(Array(vec![Integer(1)]) < Array(vec![Integer(2)]));
+        assert!(Array(vec![Integer(2)]) == Array(vec![Integer(2)]));
     }
 
     #[test]
     fn ordering_nested_data_arrays_works_as_expected() {
-        assert_eq!(
-            Array(vec![Array(vec![Integer(1)])]) < Array(vec![Array(vec![Integer(2)])]),
-            true
-        );
-        assert_eq!(
-            Array(vec![Array(vec![Integer(2)])]) == Array(vec![Array(vec![Integer(2)])]),
-            true
-        );
+        assert!(Array(vec![Array(vec![Integer(1)])]) < Array(vec![Array(vec![Integer(2)])]));
+        assert!(Array(vec![Array(vec![Integer(2)])]) == Array(vec![Array(vec![Integer(2)])]));
     }
 
     #[test]
     fn ordering_unequally_sized_arrays_works_as_expected() {
-        assert_eq!(
-            Array(vec![Integer(1), Integer(2)]) < Array(vec![Integer(2)]),
-            true
-        );
-        assert_eq!(
-            Array(vec![Integer(1)]) < Array(vec![Integer(1), Integer(2)]),
-            true
-        );
-        assert_eq!(
-            Array(vec![Integer(1)]) < Array(vec![Integer(2), Integer(1)]),
-            true
-        );
+        assert!(Array(vec![Integer(1), Integer(2)]) < Array(vec![Integer(2)]));
+        assert!(Array(vec![Integer(1)]) < Array(vec![Integer(1), Integer(2)]));
+        assert!(Array(vec![Integer(1)]) < Array(vec![Integer(2), Integer(1)]));
     }
 
     #[test]
     fn demo_example_1() {
-        assert_eq!(
+        assert!(
             Array(vec![
                 Integer(1),
                 Integer(1),
@@ -139,32 +171,29 @@ mod tests {
                 Integer(1),
                 Integer(1)
             ]),
-            true
         );
     }
 
     #[test]
     fn demo_example_2() {
-        assert_eq!(
+        assert!(
             Array(vec![
                 Array(vec![Integer(1)]),
                 Array(vec![Integer(2), Integer(3), Integer(4)]),
             ]) < Array(vec![Array(vec![Integer(1)]), Integer(4)]),
-            true
         );
     }
 
     #[test]
     fn demo_example_3() {
-        assert_eq!(
-            Array(vec![Integer(9)]) < Array(vec![Array(vec![Integer(8), Integer(7), Integer(6)])]),
-            false
+        assert!(
+            Array(vec![Integer(9)]) > Array(vec![Array(vec![Integer(8), Integer(7), Integer(6)])])
         );
     }
 
     #[test]
     fn demo_example_4() {
-        assert_eq!(
+        assert!(
             Array(vec![
                 Array(vec![Integer(4), Integer(4)]),
                 Integer(4),
@@ -175,29 +204,50 @@ mod tests {
                 Integer(4),
                 Integer(4)
             ]),
-            true
         );
     }
 
     #[test]
     fn demo_example_5() {
-        assert_eq!(
+        assert!(
             Array(vec![Integer(7), Integer(7), Integer(7), Integer(7)])
-                < Array(vec![Integer(7), Integer(7), Integer(7)]),
-            false
+                > Array(vec![Integer(7), Integer(7), Integer(7)])
         );
     }
 
     #[test]
     fn demo_example_6() {
-        assert_eq!(Array(vec![]) < Array(vec![Integer(3)]), true);
+        assert!(Array(vec![]) < Array(vec![Integer(3)]));
     }
 
     #[test]
     fn demo_example_7() {
+        assert!(Array(vec![Array(vec![Array(vec![])])]) > Array(vec![Array(vec![])]));
+    }
+
+    #[test]
+    fn parses_array_of_numbers() {
         assert_eq!(
-            Array(vec![Array(vec![Array(vec![])])]) < Array(vec![Array(vec![])]),
-            false
-        );
+            Array(vec![Integer(1), Integer(2), Integer(10), Integer(0)]),
+            "[1,2,10,0]".parse().unwrap()
+        )
+    }
+
+    #[test]
+    fn parses_empty_array() {
+        assert_eq!(Array(vec![]), "[]".parse().unwrap())
+    }
+
+    #[test]
+    fn parses_nested_arrays() {
+        assert_eq!(
+            Array(vec![
+                Integer(1),
+                Array(vec![]),
+                Array(vec![Integer(1)]),
+                Array(vec![Array(vec![Integer(1)])])
+            ]),
+            "[1,[],[1],[[1]]]".parse().unwrap()
+        )
     }
 }
